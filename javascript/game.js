143fcf0fba1f013425338af58a9b2b58eff7ca44
Broc Atkinson <44753400+BrocAtkinson@users.noreply.github.com>
2019-07-15 19:59:52 -0600

    Add files via upload

---
+++ javascript/game.js
@@ -0,0 +1,42 @@
+// random number generators for game  
+$( document ).ready(function(){
+var randomNum = Math.floor((Math.random() * 100) + 19);
+$('#randomNumber').text(Random);
+// random number generators for gems   
+var r1 = Math.floor((Math.random() * 12) + 1);
+var r2 = Math.floor((Math.random() * 12) + 1);
+var r3 = Math.floor((Math.random() * 12) + 1);
+var r4 = Math.floor((Math.random() * 12) + 1);
+var loss;
+var  win;
+var  user;
+
+
+// need 4 crystals 
+r1.addEventListener('click', function()
+ {
+console.log("hey you clicked on r1");
+})
+r2_div.addEventListener('click', function()
+ {
+console.log("hey you clicked on r2");
+})
+r3_div.addEventListener('click', function()
+ {
+console.log("hey you clicked on r3");
+})
+r4_div.addEventListener('click', function()
+ {
+console.log("hey you clicked on r4");
+})
+// random number generator for each crystal 1-12 
+
+// random number generator for score to match for game 1-90
+
+// game needs to reset after each win and loss 
+
+// event when the player goes over the random # 
+
+// computer score 
+
+// user score if equals random # true 
